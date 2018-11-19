0e48ffeba4365f65a36762791d965e99cb65eecc
saccoa <als.alsx@gmail.com>
2018-11-19 18:08:00 -0300

    access_token is not used anymore

--- src/views/JuntarFirmas.vue
+++ src/views/JuntarFirmas.vue
@@ -218,7 +218,7 @@ export default {
           if (response.authResponse) {
               console.log('Welcome!  Fetching your information.... ');
               //console.log(response); // dump complete info
-              access_token = response.authResponse.accessToken; //get access token
+              //access_token = response.authResponse.accessToken; //get access token
               user_id = response.authResponse.userID; //get FB UID
 
               FB.api('/me', function(response) {
